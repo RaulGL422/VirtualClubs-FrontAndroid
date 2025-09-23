f6cf4fa83307b39bab8b509b4f965bbc2100f748
Raul-FPGS <raul.galindo.lopez.alu@iesfernandoaguilar.es>
2025-09-23 22:38:37 +0200

    Updated activities

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,12 +33,12 @@ android {
     productFlavors {
         create("dev") {
             dimension = "env"
-            buildConfigField("String", "BASE_URL", "\"https://api.virtualclubs.local:62496/\"")
+            buildConfigField("String", "BASE_URL", "\"https://virtualclubs-backend.onrender.com/\"")
             buildConfigField("String", "GOOGLE_CLIENT_ID", "\"879741762461-03ck5fqsbrrpgs6gvidvdofpe6ebao6u.apps.googleusercontent.com\"")
         }
         create("prod") {
             dimension = "env"
-            buildConfigField("String", "BASE_URL", "\"https://api.virtualclubs.local:62496/\"")
+            buildConfigField("String", "BASE_URL", "\"https://virtualclubs-backend.onrender.com/\"")
             buildConfigField("String", "GOOGLE_CLIENT_ID", "\"${localProperties["GOOGLE_CLIENT_ID"]}\"")
         }
     }
