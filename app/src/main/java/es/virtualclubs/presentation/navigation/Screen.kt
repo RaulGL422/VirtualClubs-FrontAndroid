47b43d3b1985072f7ded3519ba539c731726cd58
Raul-FPGS <raul.galindo.lopez.alu@iesfernandoaguilar.es>
2025-12-05 20:00:04 +0100

    HotFix

--- app/src/main/java/es/virtualclubs/presentation/navigation/Screen.kt
+++ app/src/main/java/es/virtualclubs/presentation/navigation/Screen.kt
@@ -5,6 +5,6 @@ sealed class Screen(val route: String, val name: String) {
     object Settings : Screen("settings", "settings_page")
     object ResetPassword : Screen("reset-password/{token}", "reset_password_page")
     object VerifyEmail : Screen("verify_email", "verify_email_page")
-    object VerifyEmailResult : Screen("verify_email_result/{token}", "verify_email_result_page")
+    object VerifyEmailResult : Screen("verify_email_result/{result}", "verify_email_result_page")
     object Home: Screen("home", "home_page")
 }
